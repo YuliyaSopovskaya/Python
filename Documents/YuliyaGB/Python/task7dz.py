08f689ce89c08e7f57f18e913f048512867ef979
“Yuliya” <enjoy2121@yandex.ru>
2022-09-25 23:12:52 +0300

    add

--- Documents/YuliyaGB/Python/task7dz.py
+++ Documents/YuliyaGB/Python/task7dz.py
@@ -1,9 +1,22 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z 
 # для всех значений предикат.
 
-print ('x y z')
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            if not(x or y or z) == (not x and not y and not z):
-                print ('x y z')
+def inputNumb(x):
+    xyz = ["x", "y", "z"]
+    a = []
+    for i in range(x):
+        a.append(input(f"Введите значение {xyz[i]}: "))
+    return a
+
+def predicate(x):
+    left = not (x[0] or x[1] or x[2])
+    right = not x[0] and not x[1] and not x[2]
+    result = left == right
+    return result
+
+statement = inputNumb(3)
+
+if predicate(statement) == True:
+    print(f"Утверждение истинно")
+else:
+    print(f"Утверждение ложно")
