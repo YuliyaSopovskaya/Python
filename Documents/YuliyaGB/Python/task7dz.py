2d51db49a40c64d2c00baeb938dc312a0ccb84fe
“Yuliya” <enjoy2121@yandex.ru>
2022-09-27 22:17:58 +0300

    task7

--- Documents/YuliyaGB/Python/task7dz.py
+++ Documents/YuliyaGB/Python/task7dz.py
@@ -1,22 +1,31 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z 
 # для всех значений предикат.
 
-def inputNumb(x):
-    xyz = ["x", "y", "z"]
-    a = []
-    for i in range(x):
-        a.append(input(f"Введите значение {xyz[i]}: "))
-    return a
+# def inputNumb(x):
+#     xyz = ["x", "y", "z"]
+#     a = []
+#     for i in range(x):
+#         a.append(input(f"Введите значение {xyz[i]}: "))
+#     return a
 
-def predicate(x):
-    left = not (x[0] or x[1] or x[2])
-    right = not x[0] and not x[1] and not x[2]
-    result = left == right
-    return result
+# def predicate(x):
+#     left = not (x[0] or x[1] or x[2])
+#     right = not x[0] and not x[1] and not x[2]
+#     result = left == right
+#     return result
 
-statement = inputNumb(3)
+# statement = inputNumb(3)
 
-if predicate(statement) == True:
-    print(f"Утверждение истинно")
-else:
-    print(f"Утверждение ложно")
+# if predicate(statement) == True:
+#     print(f"Утверждение истинно")
+# else:
+#     print(f"Утверждение ложно")
+
+input()
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print(not (x or y or z) == (not x and not y and not z))
+            print(x, y, z)
+
+print('Для всех значений предикат утверждение истинно')
