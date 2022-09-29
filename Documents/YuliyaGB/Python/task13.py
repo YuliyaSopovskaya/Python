1d63f3de6cd05e32a6547af0f655e5d44f2149aa
“Yuliya” <enjoy2121@yandex.ru>
2022-09-29 19:56:35 +0300

    task14

---
+++ Documents/YuliyaGB/Python/task13.py
@@ -0,0 +1,20 @@
+# 3. Напишите программу, в которой пользователь будет задавать две строки,
+#  а программа - определять количество вхождений одной строки в другой.
+
+str_one = 'good news everyone, or one'
+str_two = 'one'
+
+#S.find(str, [start],[end])
+
+count = 0
+index = 0
+start = index
+while index != -1:
+    if str_one.find(str_two, start+1) != -1:
+        index = str_one.find(str_two, start+1)
+        start = index
+        count += 1
+    else:
+        index = -1
+
+print(count)
