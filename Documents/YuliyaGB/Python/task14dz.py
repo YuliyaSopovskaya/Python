ec0bf1b31e45ccfc3544dec9d1ebaca9ba60b6b1
“Yuliya” <enjoy2121@yandex.ru>
2022-10-01 20:31:03 +0300

    task14end

--- Documents/YuliyaGB/Python/task14dz.py
+++ Documents/YuliyaGB/Python/task14dz.py
@@ -5,11 +5,29 @@
 # - 6782 -> 23
 # - 0,56 -> 11
 
-num = int(input('Введите число: '))
-sum = 0
+# num = int(input('Введите число: '))
+# sum = 0
 
-while num > 0 and num < 0:
-  sum += num % 10
-  num //= 10
+# while num > 0 and num < 0:
+#   sum += num % 10
+#   num //= 10
 
-print(sum)
+# print(sum)
+
+def func(number):
+    sum = 0
+
+    number = number.replace(',', '.')
+
+    for symbol in number:
+        if symbol != '.':
+            sum += int(symbol)
+
+    return sum
+
+
+if __name__ == "_main_":
+    chislo = input('Input a number: ')
+    summary = func(chislo)
+
+    print(summary)
